bbac37bfe24aca467668d14e31f1dc12d6d371df
miguel-dev <miguelacortes@zoho.com>
2021-02-04 23:17:12 -0500

    [Task] 8. Clock's ticking

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    private double timer;
+    private double minutes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        minutes = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        double seconds = Math.Round(timer, 2);
+        if (seconds == 60.00)
+        {
+            timer = 0;
+            minutes++;
+        }
+        string min = minutes.ToString();
+        string sec = seconds.ToString(System.Globalization.CultureInfo.InvariantCulture);
+        TimerText.text = $"{min}:{sec}";
+    }
+}
