51922149a6f9f48cc807cdb4731e72fdc1ff72eb
miguel-dev <miguelacortes@zoho.com>
2021-02-12 00:00:55 -0500

    [Task] 0. Leveling up

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController control;
+    public GameObject mainCamera;
+    private float moveHorizontal;
+    private float moveVertical;
+    private float speed;
+    private float jumpSpeed;
+    private float gravity;
+    private Vector3 movement = Vector3.zero;
+    public Text TimerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GetComponent<CharacterController> ();
+        speed = 7f;
+        jumpSpeed = 9f;
+        gravity = 21f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = transform.position - mainCamera.transform.position;
+        relativePos.Normalize();
+        relativePos.y = 0;
+
+        if (control.isGrounded) {
+            moveHorizontal = Input.GetAxisRaw("Horizontal");
+            moveVertical = Input.GetAxisRaw("Vertical");
+            movement = (moveHorizontal * mainCamera.transform.right) + (moveVertical * relativePos);
+            movement = transform.TransformDirection(movement);
+            movement.Normalize();
+            movement *= speed;
+            if (Input.GetButton("Jump"))
+                movement.y = jumpSpeed;
+        }
+        movement.y -= gravity * Time.deltaTime;
+        control.Move(movement * Time.deltaTime);
+        
+        if (transform.position.y < -21)
+        {
+            transform.position = new Vector3(0f, 30f, 0f);
+            movement = Vector3.zero;
+            GetComponent<Timer>().enabled = false;
+            TimerText.text = "0:00.00";
+            TimerText.color = Color.white;
+            TimerText.fontSize = 48;
+        }
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Text time;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<Timer>().enabled = false;
+            time.fontSize = 60;
+            time.color = Color.green;
+        }
+    }
+}
