a0a57d1873ad0cc2d7436bb2ded95cbcceec5a4c
miguel-dev <miguelacortes@zoho.com>
2021-02-28 22:34:47 -0500

    [Task] 2. Transitions

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject player;
+    public Canvas timerCanvas;
+
+    void Finish()
+    {
+        mainCamera.gameObject.SetActive(true);
+        player.GetComponent<PlayerController>().enabled = true;
+        timerCanvas.gameObject.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
